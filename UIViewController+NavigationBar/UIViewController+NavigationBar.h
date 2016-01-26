5b0bd13122168687566ef444b3b8a74c382cd802
Suyeol Jeon <devxoul@gmail.com>
2016-01-27 02:21:44 +0900

    Start Xcode project

---
+++ UIViewController+NavigationBar/UIViewController+NavigationBar.h
@@ -0,0 +1,19 @@
+//
+//  UIViewController+NavigationBar.h
+//  UIViewController+NavigationBar
+//
+//  Created by 전수열 on 1/27/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewController+NavigationBar.
+FOUNDATION_EXPORT double UIViewController_NavigationBarVersionNumber;
+
+//! Project version string for UIViewController+NavigationBar.
+FOUNDATION_EXPORT const unsigned char UIViewController_NavigationBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewController_NavigationBar/PublicHeader.h>
+
+
---
+++ UIViewController+NavigationBarTests/UIViewController_NavigationBarTests.m
@@ -0,0 +1,39 @@
+//
+//  UIViewController_NavigationBarTests.m
+//  UIViewController+NavigationBarTests
+//
+//  Created by 전수열 on 1/27/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIViewController_NavigationBarTests : XCTestCase
+
+@end
+
+@implementation UIViewController_NavigationBarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
